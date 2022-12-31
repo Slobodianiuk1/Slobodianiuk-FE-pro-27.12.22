81797a319ed7b40bea7f3b0cdd7595b7181e62fc
vladislavslobodanuk <vladslobodanuk9@gmail.com>
2022-12-31 10:26:05 +0200

    add HW-4 (Create a small survey)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('Hello world!')
+let name, age, question;
+
+name = prompt('Whats your name?');
+age = +prompt('How old are you?');
+question = confirm('You smoke?');
+
+if(age >= 18 && question) {
+    alert(`Hi ${name}, well, you chose this path yourself…`);
+} else if (age >= 18 && !question) {
+    alert(`Hi ${name}, great! You probably also play sports!`);
+} else if (age < 18 && question) {
+    alert(`Hello ${name}! Do the parents know about it?`);
+} else alert(`Hello ${name}! That's right, now the main thing is to study well.`);
