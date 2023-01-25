55e7da8072344a0783b85d3024bac1dcb26c55ec
vladislavslobodanuk <vladslobodanuk9@gmail.com>
2023-01-25 11:47:03 +0200

    add HW-9 (add function 'removeElement')

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log('Hello world!')
+const arr = [1, 2, 3, 4, 5, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 4];
+
+// Видалить елемент по індексу
+function removeElement (arr, indexEl) {
+    arr.splice(indexEl, 1);
+}
+
+removeElement(arr, 5);
+console.log(arr);
+
+// Видалить всі елемети з масиву яки передать як аргумент в функицію
+function removeElement1 (arr, el) {
+    while (arr.includes(el)) {
+        let indexEl = arr.indexOf(el);
+        arr.splice(indexEl, 1);
+    }
+}
+
+removeElement1(arr, 7)
+console.log(arr)
