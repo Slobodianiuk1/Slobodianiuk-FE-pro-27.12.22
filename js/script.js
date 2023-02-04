ff404c326d982a65e0eff2e993afdc6e03f35e00
vladislavslobodanuk <vladslobodanuk9@gmail.com>
2023-02-04 17:43:03 +0200

    add HW-12 (add function 'sum')

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('Hello world!')
+function getSum () {
+  let result = 0;
+  return function(num) {
+    return result += num;
+  };
+}
+
+let sum = getSum();
+
+console.log(sum(3));
+console.log(sum(5));
+console.log(sum(20));
