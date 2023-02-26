772c94ca4cfa3dab8b74c4bba4694104c27f3c00
vladislavslobodanuk <vladslobodanuk9@gmail.com>
2023-02-26 21:44:10 +0200

    HW-15 (refactoring, fixing code)

--- js/script.js
+++ js/script.js
@@ -1,96 +1,69 @@
 function addButtonClicked (event) {
 	event.preventDefault();
+
 	const list = document.getElementById("list");
+	const inputValue = document.getElementById('newItemInput').value.trim();
 
-	let inputValue = document.getElementById('newItemInput');
-	if (inputValue.value === '') return;
+	if (!inputValue) return;
 
-	let newItem = document.createElement("li");
-	newItem.innerHTML = inputValue.value;
-	newItem.className = 'added-item';
+	const newItem = document.createElement("li");
+	newItem.textContent = inputValue;
+	newItem.classList.add('added-item');
 
 	list.appendChild(newItem);
 
 	newItem.addEventListener('click', function () {
 		this.classList.toggle('selected-item');
+		checkListLength()
 	});
 
+	document.getElementById('newItemInput').value = '';
+
 	checkListLength();
-	inputValue.value = '';
 	handlerChengeInput();
 }
 
 function removeButtonClicked (event) {
 	event.preventDefault();
-	let lastItem = document.getElementById("list").lastChild;
-	document.getElementById("list").removeChild(lastItem);
+	const lastItem = document.getElementById("list").lastChild;
+	lastItem && lastItem.remove();
 
 	checkListLength();
 }
 
 function checkListLength () {
-	const countList = document.getElementById("list").children.length;
+	const list = document.getElementById("list");
+	const countList = list.children.length;
 	const removeButton = document.getElementById("removeButton");
 	const deleteButton = document.querySelector('.delete-all');
 	const selectedDelete = document.querySelector('.deleteIsCompleted');
 
-	if (countList > 0) {
-		removeButton.removeAttribute('disabled');
-		deleteButton.removeAttribute('disabled');
-		selectedDelete.removeAttribute('disabled');
-		removeButton.classList.remove('button-disabled');
-		deleteButton.classList.remove('button-disabled');
-		selectedDelete.classList.remove('button-disabled');
+	const setDisabled = (button, isDisabled) => {
+		button.disabled = isDisabled;
+		button.classList.toggle('button-disabled', isDisabled);
+	};
 
-	} else {
-		removeButton.classList.add('button-disabled');
-		deleteButton.classList.add('button-disabled');
-		selectedDelete.classList.add('button-disabled');
-		removeButton.setAttribute('disabled', 'disabled');
-		deleteButton.setAttribute('disabled', 'disabled');
-		selectedDelete.setAttribute('disabled', 'disabled');
-	}
-	checkCountList()
-
+	setDisabled(removeButton, countList <= 0);
+	setDisabled(deleteButton, countList <= 0);
+	setDisabled(selectedDelete, countList <= 0 || !list.querySelector('.selected-item'));
+	document.getElementById('count-list').textContent = countList ? `Total: ${countList}` : `List is empty`;
 }
-
-
-function checkCountList () {
-	const list = document.getElementById('list').childElementCount;
-	const div = document.getElementById('count-list');
-	div.innerHTML = list ? `Total: ${list}` : `List is empty`
-}
-
 
 function deleteAll () {
-	const list = document.getElementById('list');
-	while (list.firstChild) {
-		list.removeChild(list.firstChild);
-	}
-	checkListLength()
+	document.getElementById('list').innerHTML = '';
+	checkListLength();
 }
 
 function handlerChengeInput () {
 	const input = document.getElementById('newItemInput');
 	const addButton = document.getElementById('addButton');
-	if (input.value) {
-		addButton.classList.remove('button-disabled');
-		addButton.removeAttribute('disabled');
-	} else {
-		addButton.classList.add('button-disabled');
-		addButton.setAttribute('disabled', 'disabled');
-	}
+	addButton.disabled = addButton.classList.toggle('button-disabled', !input.value);
 }
 
 function checkIsCompleted () {
-	const list = document.getElementById('list')
-	const listChildren = list.children;
-	for (let i = 0; i < listChildren.length; i++) {
-		if (listChildren[i].classList.contains('selected-item')) {
-			// listChildren[i].style.display = 'none'
-			list.removeChild(listChildren[i])
-		}
-	}
-	checkListLength()
-}
+	const list = document.getElementById('list');
+	const selectedItems = [...list.querySelectorAll('.selected-item')];
 
+	selectedItems.forEach(el => el.remove());
+	checkListLength();
+}
