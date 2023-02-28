b979f9d937dc3ad393e91ff365ac0cdb7ab839b5
vladislavslobodanuk <vladslobodanuk9@gmail.com>
2023-02-28 14:54:25 +0200

    HW-15 (fix bag & add localStorage)

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
-const tasks = [
-	{id: 1, title: 'Task 1', completed: false},
-	{id: 2, title: 'Task 2', completed: true},
-];
+document.addEventListener("DOMContentLoaded", () => {
 
+	const list = document.getElementById("list");
+	const input = document.getElementById('newItemInput');
+	const addButton = document.getElementById('addButton');
+	const removeButton = document.getElementById("removeButton");
+	const deleteButton = document.querySelector('.delete-all');
+	const selectedDelete = document.querySelector('.deleteIsCompleted');
 
-function renderList () {
-	tasks.forEach(el => {
-		const list = document.getElementById("list");
-		// const inputValue = document.getElementById('newItemInput').value.trim();
+	function addButtonClicked (event) {
+		event.preventDefault();
+		const inputValue = input.value.trim();
+		if (!inputValue) return;
 
 		const newItem = document.createElement("li");
-		newItem.textContent = el.title;
+		newItem.textContent = inputValue;
 		newItem.classList.add('added-item');
 
 		list.appendChild(newItem);
@@ -19,95 +22,84 @@ function renderList () {
 			this.classList.toggle('selected-item');
 			checkListLength()
 		});
-	})
-}
 
-renderList()
+		input.value = '';
 
-class Task {
-	constructor(id, title, completed = false) {
-		this.id = id;
-		this.title = title;
-		this.completed = completed;
+		checkListLength();
+		handlerChengeInput();
 	}
 
-	toggleCompleted() {
-		this.completed = !this.completed;
+	function removeButtonClicked (event) {
+		event.preventDefault();
+		const lastItem = document.getElementById("list").lastChild;
+		lastItem && lastItem.remove();
+
+		checkListLength();
 	}
-}
-function addItem () {
-	const inputValue = document.getElementById('newItemInput').value.trim();
-	let task = new Task
-	tasks.push[...]
-}
 
-function addButtonClicked (event) {
-	event.preventDefault();
+	function checkListLength () {
+		const countList = list.children.length;
 
-	const list = document.getElementById("list");
-	const inputValue = document.getElementById('newItemInput').value.trim();
+		const setDisabled = (button, isDisabled) => {
+			button.disabled = isDisabled;
+			button.classList.toggle('button-disabled', isDisabled);
+		};
 
-	if (!inputValue) return;
+		setDisabled(removeButton, countList <= 0);
+		setDisabled(deleteButton, countList <= 0);
+		setDisabled(selectedDelete, countList <= 0 || !list.querySelector('.selected-item'));
+		document.getElementById('count-list').textContent = countList ? `Total: ${countList}` : `List is empty`;
+	}
 
-	const newItem = document.createElement("li");
-	newItem.textContent = inputValue;
-	newItem.classList.add('added-item');
+	function deleteAll () {
+		document.getElementById('list').innerHTML = '';
+		checkListLength();
+	}
 
-	list.appendChild(newItem);
+	function handlerChengeInput () {
+		addButton.disabled = addButton.classList.toggle('button-disabled', !input.value);
+	}
 
-	newItem.addEventListener('click', function () {
-		this.classList.toggle('selected-item');
+	function checkIsCompleted () {
+		const selectedItems = list.querySelectorAll('.selected-item');
+
+		selectedItems.forEach(el => el.remove());
+		checkListLength();
+	}
+
+	function saveListToLocalStorage () {
+		const list = document.getElementById('list');
+
+		const listHtml = list.innerHTML;
+		localStorage.setItem('listHtml', listHtml);
+	}
+
+
+
+	const listHtml = localStorage.getItem('listHtml');
+	if (listHtml) {
+		const list = document.getElementById('list');
+		list.innerHTML = listHtml;
+		[...list.children].forEach(el => {
+			el.addEventListener('click', () => {
+				el.classList.toggle('selected-item');
+				checkListLength()
+			})
+		})
+
 		checkListLength()
-	});
 
-	document.getElementById('newItemInput').value = '';
+	}
 
-	checkListLength();
-	handlerChengeInput();
-}
+	window.addEventListener('beforeunload', saveListToLocalStorage);
+	input.addEventListener('input', handlerChengeInput);
+	removeButton.addEventListener('click', (event) => removeButtonClicked(event));
+	addButton.addEventListener('click', (event) => addButtonClicked(event));
+	deleteButton.addEventListener('click', deleteAll);
+	selectedDelete.addEventListener('click', checkIsCompleted);
+});
 
-function removeButtonClicked (event) {
-	event.preventDefault();
-	const lastItem = document.getElementById("list").lastChild;
-	lastItem && lastItem.remove();
 
-	checkListLength();
-}
 
-function checkListLength () {
-	const list = document.getElementById("list");
-	const countList = list.children.length;
-	const removeButton = document.getElementById("removeButton");
-	const deleteButton = document.querySelector('.delete-all');
-	const selectedDelete = document.querySelector('.deleteIsCompleted');
 
-	const setDisabled = (button, isDisabled) => {
-		button.disabled = isDisabled;
-		button.classList.toggle('button-disabled', isDisabled);
-	};
-
-	setDisabled(removeButton, countList <= 0);
-	setDisabled(deleteButton, countList <= 0);
-	setDisabled(selectedDelete, countList <= 0 || !list.querySelector('.selected-item'));
-	document.getElementById('count-list').textContent = countList ? `Total: ${countList}` : `List is empty`;
-}
-
-function deleteAll () {
-	document.getElementById('list').innerHTML = '';
-	checkListLength();
-}
-
-function handlerChengeInput () {
-	const input = document.getElementById('newItemInput');
-	const addButton = document.getElementById('addButton');
-	addButton.disabled = addButton.classList.toggle('button-disabled', !input.value);
-}
-
-function checkIsCompleted () {
-	const list = document.getElementById('list');
-	const selectedItems = [...list.querySelectorAll('.selected-item')];
-
-	selectedItems.forEach(el => el.remove());
-	checkListLength();
-}
 
