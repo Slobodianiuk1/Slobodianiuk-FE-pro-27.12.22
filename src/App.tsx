36086867cb1469b589c0e98ce3206a46863bc44d
vladislavslobodanuk <vladslobodanuk9@gmail.com>
2023-05-06 13:21:11 +0300

    updated the basic project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {FC} from "react";
+
+const App: FC = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </div>
+    );
+};
+
+export default App;
