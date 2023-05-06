18cda101468c219e386f05c5f7996f939cc6ef69
vladislavslobodanuk <vladslobodanuk9@gmail.com>
2023-05-06 11:58:21 +0300

    HW-22

--- src/components /UI/Button/Button.tsx
+++ src/components /UI/Button/Button.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
 import clsx from 'clsx';
 import { ACTIONS, IReducerAction } from '../../../store/reducer';
 import { IParams } from '../../../types/hamburger';
